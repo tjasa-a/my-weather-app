0f9772fb4fe2a04dfb36a554a0f1b54ead63919f
tjasa-a <147998238+tjasa-a@users.noreply.github.com>
2023-10-21 14:28:39 +0200

    Added forecast functionality

--- src/script.js
+++ src/script.js
@@ -29,6 +29,56 @@ let dateElement = document.querySelector("#date");
 let currentTime = new Date();
 dateElement.innerHTML = formatDate(currentTime);
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+//Forecast
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index > 0) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2">
+          <div class="weather-forecast-date">${formatDay(
+            forecastDay.time
+          )}</div>
+          <img src= "${forecastDay.condition.icon_url}" alt="${
+          forecastDay.condition.icon
+        }" width="42" />
+          <div class="wather-forecast-temperature">
+            <span class="weather-forecast-temperature-max"> ${Math.round(
+              forecastDay.temperature.maximum
+            )}° </span>
+            <span class="weather-forecast-temperature-min"> ${Math.round(
+              forecastDay.temperature.minimum
+            )}° </span>
+          </div>
+        </div>
+      `;
+    }
+  });
+  forecastHTML = forecastHTML + `</div>`;
+
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "03tfb48099c19775o01bafce7bdf4d20";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.longitude}&lat=${coordinates.latitude}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 //Display weather
 
 function displayWeather(response) {
@@ -53,6 +103,8 @@ function displayWeather(response) {
 
   let displayWeather = document.querySelector("#weather-display");
   displayWeather.style.display = "block";
+
+  getForecast(response.data.coordinates);
 }
 
 // Search engine
